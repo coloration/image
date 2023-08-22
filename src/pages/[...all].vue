101c698c575990480a14681ea4a5294bab397b3b
Binyu <binyu1231@gmail.com>
2023-08-22 15:42:59 +0800

    fix: for netify build

--- src/pages/[...all].vue
+++ src/pages/[...all].vue
@@ -1,85 +1,19 @@
 <script lang="ts" setup>
-import { ref } from 'vue'
-import init, { grayscale } from 'frame-handler'
-
-const canvasRef = ref<any>(null)
-
-const imgs = ref<string[]>([])
-
-async function handleFileChange(e: any) {
-  console.log('files', e.target.files)
-  const files: File[] = Array.from(e.target.files)
-  const reader = new FileReader()
-  const fileBuffers = await files.reduce(async (last, f) => {
-    const allUint8Data = await last
-    console.log(f)
-
-    
-    return new Promise((resolve) => {
-      reader.onload = () => {
-        const uint8Data = new Uint8ClampedArray(reader.result as ArrayBuffer)
-        allUint8Data.push(uint8Data)
-        resolve(allUint8Data)
-      }
-      reader.readAsArrayBuffer(f)
-
-     
-    })
-  }, Promise.resolve([] as Uint8ClampedArray[]))
-
-  console.log('ffff')
-
-  console.log(fileBuffers)
-
-  await init()
-
-  imgs.value = fileBuffers.map((b) => grayscale(b as any))
-
-
-
-  const ctx = canvasRef.value.getContext('2d')
-
-  console.log(ctx)
-
-  const img = new Image()
-  img.onload = () => {
-    ctx.drawImage(img, 0, 0)
-  }
-  const base64Reader = new FileReader()
-  base64Reader.onload = () => {
-    img.src = base64Reader.result as string
-  }
-  base64Reader.readAsDataURL(files[0])
-  
-  
-
-  const photon = await import('@silvia-odwyer/photon')
-  
-  let newImage = photon.open_image(canvasRef.value, ctx);
-  photon.filter(newImage, "radio");
-
-  photon.putImageData(canvasRef.value, ctx, newImage);
-}
 
 </script>
+
 <template>
-<div class="home">
+  <div class="text-center">
+    Not Found 
 
-  <label for="file">
-    <input type="file" id="file" accept = 'image/*' multiple @change="handleFileChange">
+    <div></div>
 
-    <div class="w-100 h-80 bg-blue-400">
-      <div v-for="(img, i) in imgs" :key="i">
-        <img :src="img" alt="">
-      </div>
-    </div>
-
-    <canvas ref="canvasRef" class="h-100px w-100px"></canvas>
-  </label>
-</div>
+    <OptButton>
+      back home
+    </OptButton>
+  </div>
 </template>
+
 <style lang="postcss">
-.home {
-  @apply;
-}
+
 </style>
